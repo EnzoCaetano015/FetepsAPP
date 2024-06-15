a6b05311363636a9c6b0e726eb9139c8de9c490e
EnzoCaetano015 <peracioenzo@gmail.com>
2024-06-14 23:50:29 -0300

    app copilado apenas na vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:feteps/SplashScreen_page.dart';
 // import 'package:feteps/telainicial_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
